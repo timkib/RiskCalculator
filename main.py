784e19a55bf58d29cf9f35183a82c6c6c285c552
timkib <92236550+timkib@users.noreply.github.com>
2024-01-17 21:39:57 +0100

    Update main.py
    added number of desired days for VaR and ES

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ class VarCov:
         self.mu_hat = 0
         self.cov_hat = 0
 
-    def fit(self, x, w, alpha=0.99):
+    def fit(self, x, w, alpha=0.99, d=1):
         """Fits the model to the (portfolio) data and returns VaR and ES. \n
         X is the log-difference matrix of the form (N x A ) with A=number of assets.
         Alpha: the corresponding confidence level, between 0 and 1. The standard is 0.99.\n
-        w: The weights / price vector. With shape (A x 1) E.g. (120, 90, 80).T"""
+        w: The weights / price vector. With shape (A x 1) E.g. (120, 90, 80).T \n
+        d: d days VaR and ES, respectively.
+        The model uses the linearized loss operator."""
 
         num_assets = x.shape[1]
         n = x.shape[0]
         self.mu_hat = np.mean(x, axis=0).reshape((num_assets, 1))
         self.cov_hat = np.cov(x, rowvar=False)
 
-        VaR = -w.T @ self.mu_hat + np.sqrt(w.T @ self.cov_hat @ w) * norm.ppf(alpha)
-        ES = -w.T @ self.mu_hat + np.sqrt(w.T @ self.cov_hat @ w) * norm.pdf(norm.ppf(alpha))/(1 - alpha)
+        VaR = -w.T @ (self.mu_hat * d) + np.sqrt(w.T @ (d * self.cov_hat) @ w) * norm.ppf(alpha)
+        ES = -w.T @ (self.mu_hat * d) + np.sqrt(w.T @  (d* self.cov_hat) @ w) * norm.pdf(norm.ppf(alpha))/(1 - alpha)
         return VaR.item(0), ES.item(0)
-
